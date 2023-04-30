b2457d5fb386d18880bca2c99c64d7edfbba767f
nathan <nathanberger.rs@hotmail.com>
2023-04-29 23:22:24 -0300

    fix(UserModule): removing UserModule import from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BoardModule } from './board/board.module';
 import { ConfigModule } from '@nestjs/config';
 import { BD_PATH_ENV } from './constants/env.constant';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -11,7 +10,6 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({ envFilePath: [BD_PATH_ENV], isGlobal: true }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     BoardModule,
-    UserModule,
     AuthModule,
   ],
 })
