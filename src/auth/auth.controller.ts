55ab3c01e26d861768f746ea3eac0ce3586f3247
Diuliano Vargas da Silveira <diulidsvargas@gmail.com>
2023-05-03 14:02:32 -0300

    (feat-base-auth-module) adding public decorator method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,12 +10,14 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.email, signInDto.password);
