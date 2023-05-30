91807ce9f87c1d7ce5ba84650f8875abc8b08280
nathanmintegui <nathanberger.rs@hotmail.com>
2023-05-30 13:53:51 -0300

    feat(sign-up): add sign up endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,11 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 
+import { PrismaClient } from '@prisma/client';
+import { SignUpDto } from './dto/sign-up.dto';
+
+const prisma = new PrismaClient();
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -9,16 +14,31 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, pass: string): Promise<any> {
-    const user = await this.userService.findOne(email);
-    if (user?.name !== pass) {
-      throw new UnauthorizedException();
-    }
+  // TODO fix name and email problem
+  //  async signIn(email: string, pass: string): Promise<any> {
+  //  const user = await this.userService.findOne(email);
+  //if (user?.name !== pass) {
+  //  throw new UnauthorizedException();
+  // }
 
-    const payload = { email: user.email };
+  //const payload = { email: user.email };
 
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+  // return {
+  // access_token: await this.jwtService.signAsync(payload),
+  //};
+  //}
+
+  async signUp(req: SignUpDto) {
+    const user = await prisma.user.create({
+      data: {
+        email: req.email,
+        name: req.name,
+        password: req.password,
+        nickname: req.nickname,
+        profilePhoto: req.profilePhoto,
+      },
+    });
+
+    return user;
   }
 }
